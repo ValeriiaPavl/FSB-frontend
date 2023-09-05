9dfdda2d4b77c2195fc5b0a4dba530ee20d1c530
Valeriia Pavlova <happyamanita@gmail.com>
2023-09-05 08:42:31 +0200

    added  user coordinates on the map from db

--- src/components/DynamicMap.tsx
+++ src/components/DynamicMap.tsx
@@ -1,13 +1,32 @@
 import { useState, useEffect } from "react";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+import { z } from "zod";
 
-const DynamicMap = () => {
+const userCoordinatesValidator = z.object({
+  city_of_residence_latitude: z.string(),
+  city_of_residence_longitude: z.string(),
+});
+
+type userCoordinates = z.infer<typeof userCoordinatesValidator>;
+
+const DynamicMap = ({
+  city_of_residence_latitude,
+  city_of_residence_longitude,
+}: userCoordinates) => {
   useEffect(() => {
-    const map = L.map("map").setView([51.505, -0.09], 13);
+    const map = L.map("map").setView([52, 6], 8);
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution: "© OpenStreetMap contributors",
     }).addTo(map);
+
+    const marker = L.marker([
+      parseFloat(city_of_residence_latitude),
+      parseFloat(city_of_residence_longitude),
+    ])
+      .addTo(map)
+      .bindPopup("Here I live")
+      .openPopup();
 
     return () => {
       map.remove();
