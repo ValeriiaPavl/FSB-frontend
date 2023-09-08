11224ca185bb8caf1d7df24271d8fa824bff2d98
Valeriia Pavlova <happyamanita@gmail.com>
2023-09-08 16:30:38 +0200

    change map for getting coords from user

--- src/components/DynamicMap.tsx
+++ src/components/DynamicMap.tsx
@@ -1,39 +1,81 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, ReactEventHandler, SetStateAction } from "react";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import { z } from "zod";
 
-const userCoordinatesValidator = z.object({
-  city_of_residence_latitude: z.string(),
-  city_of_residence_longitude: z.string(),
-});
+interface MapProps {
+  location: number[] | null;
+  setLocation: React.Dispatch<React.SetStateAction<number[] | null>>;
+}
 
-type userCoordinates = z.infer<typeof userCoordinatesValidator>;
+import "leaflet/dist/leaflet.css";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
 
-const DynamicMap = ({
-  city_of_residence_latitude,
-  city_of_residence_longitude,
-}: userCoordinates) => {
+const DynamicMap = ({ location, setLocation }: MapProps) => {
+  // const [location, setLocation] = useState([51.505, -0.09]); // Default to some location
+  const [isLocationFound, setIsLocationFound] = useState(false);
+
   useEffect(() => {
-    const map = L.map("map").setView([52, 6], 8);
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution: "© OpenStreetMap contributors",
-    }).addTo(map);
-
-    const marker = L.marker([
-      parseFloat(city_of_residence_latitude),
-      parseFloat(city_of_residence_longitude),
-    ])
-      .addTo(map)
-      .bindPopup("Here I live")
-      .openPopup();
-
-    return () => {
-      map.remove();
-    };
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        function (position) {
+          setLocation([position.coords.latitude, position.coords.longitude]);
+          setIsLocationFound(true);
+        },
+        function (error) {
+          console.error("Error obtaining geolocation: ", error);
+        }
+      );
+    } else {
+      console.log("Geolocation is not available in this browser");
+    }
   }, []);
 
-  return <div id="map" style={{ width: "100%", height: "400px" }}></div>;
+  const handleMapClick = (e: L.LeafletMouseEvent) => {
+    const { lat, lng } = e.latlng;
+    console.log(`Map clicked at Latitude ${lat}, Longitude ${lng}`);
+    setLocation([lat, lng]);
+  };
+
+  return (
+    <div className="w-full h-[400px]">
+      {isLocationFound ? (
+        <MapContainer
+          className="h-80 w-full"
+          center={location}
+          zoom={13}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={location}>
+            <Popup>You are here!</Popup>
+          </Marker>
+          <MapClickHandler handler={handleMapClick} />
+        </MapContainer>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+interface MapClickHandlerProps {
+  handler: (e: L.LeafletMouseEvent) => void;
+}
+const MapClickHandler = (props: MapClickHandlerProps) => {
+  const map = useMapEvents({
+    click: props.handler,
+  });
+  return null;
 };
 
 export default DynamicMap;
