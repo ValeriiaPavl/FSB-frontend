3fb9aa523bb67a00f7d99dd107906d1a6b2707ba
Valeriia Pavlova <happyamanita@gmail.com>
2023-09-15 14:33:32 +0200

    deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,1 +1,12 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
+const Index = () => {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/users");
+  }, [router]);
+  return <p>redirecting</p>;
+};
+
+export default Index;
