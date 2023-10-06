d35f962b8d507e29ec6c40817d9ce3ae790abe1e
Valeriia Pavlova <happyamanita@gmail.com>
2023-10-06 23:23:38 +0200

    made a Menu for mobiles

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,19 +10,18 @@ interface NavWithTokenProps {
   children?: ReactNode;
 }
 
-const NavWithToken = (props: NavWithTokenProps) => {
-  const [checked, setChecked] = useState<"noToken" | "hasToken">("noToken");
+const MenuList = () => {
+  const [isToken, setIsToken] = useState<boolean>(false);
   const [token, setToken] = useState<String | null>(null);
   const [userIdFromToken, setUserId] = useState<number | null>(null);
   const [currUser, setCurrUser] = useState<User | null>(null);
   const router = useRouter();
-
   useEffect(() => {
     const tokenFromLs = localStorage.getItem("token");
     if (!tokenFromLs) {
-      setChecked("noToken");
+      setIsToken(false);
     } else {
-      setChecked("hasToken");
+      setIsToken(true);
     }
   }, []);
 
@@ -67,84 +66,172 @@ const NavWithToken = (props: NavWithTokenProps) => {
 
   const handleLogout = () => {
     localStorage.removeItem("token");
-    setChecked("noToken");
+    setIsToken(false);
     router.push("/login");
   };
 
   return (
-    <div className="flex sticky top-0 z-10 h-40 items-stretch">
+    <ul className="text-lg font-semibold text-blue">
+      {isToken ? (
+        <>
+          {" "}
+          <li>
+            <Button variant="link">
+              <Link
+                className="text-lg font-semibold text-[#f5deb3]"
+                href={`/users/extended/${userIdFromToken}`}
+              >
+                <p>Your profile</p>
+              </Link>
+            </Button>
+          </li>
+          <li>
+            <Button variant="link">
+              <Link
+                className="text-lg font-semibold text-[#f5deb3]"
+                href="/users"
+                // className="text-lg font-semibold text-[#f5deb3]"
+              >
+                <p>All users</p>
+              </Link>
+            </Button>
+          </li>
+          <li>
+            <Button variant="link">
+              <Link
+                href="/likes/from"
+                className="text-lg font-semibold text-[#f5deb3]"
+              >
+                <p>Likes from you</p>
+              </Link>
+            </Button>
+          </li>
+          <li>
+            <Button variant="link">
+              <Link
+                href="/likes/to"
+                className="text-lg font-semibold text-[#f5deb3]"
+              >
+                <p>Who liked you</p>
+              </Link>
+            </Button>
+          </li>
+          <li>
+            <Button variant="link">
+              <Link
+                href="/likes/mutual"
+                className="text-lg font-semibold text-[#f5deb3]"
+              >
+                <p>Mutual likes</p>
+              </Link>
+            </Button>
+          </li>
+          <li className="mt-4 ml-4">
+            <Button variant="destructive" onClick={handleLogout}>
+              Log Out
+            </Button>
+          </li>
+        </>
+      ) : (
+        <>
+          <li>
+            <Button variant="link">
+              <Link
+                href="/login"
+                className="text-lg font-semibold text-[#f5deb3]"
+              >
+                Login
+              </Link>
+            </Button>
+          </li>
+          <li>
+            <Button variant="link">
+              <Link
+                href="/register"
+                className="text-lg font-semibold text-[#f5deb3]"
+              >
+                Register
+              </Link>
+            </Button>
+          </li>
+        </>
+      )}
+    </ul>
+  );
+};
+
+const NavWithToken = (props: NavWithTokenProps) => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  return (
+    <div className="flex sticky top-0 z-10 h-1/6 items-stretch">
       <nav className="flex flex-grow text-[#f5deb3] bg-[#124d6a] items-center">
         <div className="px-2 py-2 relative flex items-center justify-between content-center w-full flex-wrap">
           <span className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
             ⭐ FSB FindSomeBuddy
           </span>
-          {checked === "hasToken" ? (
-            <>
-              {" "}
-              <a href={`/users/extended/${userIdFromToken}`}>
-                <Avatar className="w-20 h-20 border-4 border-[#f5deb3] hover:scale-110">
-                  <AvatarImage src={currUser?.user_avatar} />
-                  <AvatarFallback>{currUser?.username}</AvatarFallback>
-                </Avatar>
-              </a>
-              <Button variant="link">
-                <Link
-                  href="/users"
-                  className="text-lg font-semibold text-[#f5deb3]"
+          <section className="MOBILE-MENU flex lg:hidden mt-4 mb-4">
+            <div
+              className="HAMBURGER-ICON space-y-2"
+              onClick={() => setIsNavOpen((prev) => !prev)}
+            >
+              <span className="block h-0.5 w-8 animate-pulse bg-[#f5deb3]"></span>
+              <span className="block h-0.5 w-8 animate-pulse bg-[#f5deb3]"></span>
+              <span className="block h-0.5 w-8 animate-pulse bg-[#f5deb3]"></span>
+            </div>
+
+            <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+              <div
+                className="absolute top-0 right-0 px-4 py-4 text-[#f5deb3]"
+                onClick={() => setIsNavOpen(false)}
+              >
+                <svg
+                  className="h-8 w-8 "
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
-                  All users
-                </Link>
-              </Button>
-              <Button variant="link">
-                <Link
-                  href="/likes/from"
-                  className="text-lg font-semibold text-[#f5deb3]"
-                >
-                  <p>Likes from you</p>
-                </Link>
-              </Button>
-              <Button variant="link">
-                <Link
-                  href="/likes/to"
-                  className="text-lg font-semibold text-[#f5deb3]"
-                >
-                  Who liked you
-                </Link>
-              </Button>
-              <Button variant="link">
-                <Link
-                  href="/likes/mutual"
-                  className="text-lg font-semibold text-[#f5deb3]"
-                >
-                  Mutual likes
-                </Link>
-              </Button>
-              <Button variant="destructive" onClick={handleLogout}>
-                Log Out
-              </Button>
-            </>
-          ) : (
-            <>
-              <Button variant="link">
-                <Link
-                  href="/login"
-                  className="text-lg font-semibold text-[#f5deb3]"
-                >
-                  Login
-                </Link>
-              </Button>
-              <Button variant="link">
-                <Link
-                  href="/register"
-                  className="text-lg font-semibold text-[#f5deb3]"
-                >
-                  Register
-                </Link>
-              </Button>
-            </>
-          )}
+                  <line x1="18" y1="6" x2="6" y2="18" />
+                  <line x1="6" y1="6" x2="18" y2="18" />
+                </svg>
+              </div>
+              <MenuList />
+            </div>
+          </section>
+          <div className="DESKTOP-MENU flex-row hidden space-x-8 lg:flex">
+            <MenuList />
+          </div>
         </div>
       </nav>
+      <style>{`
+      .hideMenuNav {
+        display: none;
+      }
+      .showMenuNav {
+        display: block;
+        border: solid 1px light-grey;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+        border-radius: 5px;
+        position: absolute;
+        width: 50%;
+        height: 35vh;
+        top: 105%;
+        left: 5;
+        background: #124d6a;
+        text-color: blue
+        z-index: 20;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        padding-top: 4rem;
+        padding-left: 1rem;
+        color: red;
+
+      }
+    `}</style>
     </div>
   );
 };
